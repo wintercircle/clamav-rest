312f85fe182da077335e323951894a2996eb9fcf
Gianni Moschini <gianni.moschini+github@gmail.com>
2020-05-14 18:03:03 +0100

    Pass $PORT to running process as -Dserver.port

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,8 +6,8 @@ port=${CLAMD_PORT:-3310}
 filesize=${MAXSIZE:-10240MB}
 timeout=${TIMEOUT:-10000}
 
-echo "using clamd server: $host:$port"
+echo "run on $PORT, using clamd server: $host:$port"
 
 # start in background
 #java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host=$host --clamd.port=$port
-java -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host=$host --clamd.port=$port --clamd.maxfilesize=$filesize --clamd.maxrequestsize=$filesize --clamd.timeout=$timeout
+java -Dserver.port=$PORT -jar /var/clamav-rest/clamav-rest-1.0.2.jar --clamd.host=$host --clamd.port=$port --clamd.maxfilesize=$filesize --clamd.maxrequestsize=$filesize --clamd.timeout=$timeout
